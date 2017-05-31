f26cd48020ff18aa2192b1fa1beea39067722238
Shaleen Baral <shaleenbaral111@gmail.com>
2017-05-31 18:59:18 +0545

    Updated as suggested

--- Roguelike/TunnelAlgorithm.cpp
+++ Roguelike/TunnelAlgorithm.cpp
@@ -3,12 +3,11 @@
 #include "libtcod/libtcod.hpp"
 #include <algorithm>
 
-Rect::Rect(int x1, int y1, int w, int h) : x1(x1), y1(y1), x2(x1 + w), y2(x1 + h), centerX((x1+y2)/2), centerY((y1+y2)/2){
+Rect::Rect(int x1, int y1, int w, int h) : x1(x1), y1(y1), x2(x1 + w), y2(x1 + h), centerX((x1+y2)/2), centerY((y1+y2)/2){}
+Rect::Rect() : x1(NULL), y1(NULL), x2(NULL), y2(NULL), centerX(NULL), centerY(NULL){}
 
-}
-
-bool Rect::intersects(Rect *r2) {
-	return ((x1 <= r2->x2 && x2 >= r2->x1) && (y1 <= r2->y2 && r2->y2 >= r2->y1));
+bool Rect::intersects(Rect &r2) {
+	return ((x1 <= r2.x2 && x2 >= r2.x1) && (y1 <= r2.y2 && r2.y2 >= r2.y1));
 }
 
 
@@ -20,11 +19,12 @@ TunnelAlgorithm::TunnelAlgorithm(int rMax, int rMin, int rMaxRoom, int mWidth, i
 
 TunnelAlgorithm::~TunnelAlgorithm()
 {
+	delete[] mtiles;
 }
 
 Tile* TunnelAlgorithm::GenerateLevel() {
-	Rect** rooms;
-	rooms = new Rect*[ROOM_MAX_NUM];
+	Rect* rooms;
+	rooms = new Rect[ROOM_MAX_NUM];
 	int numRooms = 0;
 	for (int i = 0; i < ROOM_MAX_NUM; ++i) {
 		TCODRandom* rng = TCODRandom::getInstance();
@@ -33,12 +33,12 @@ Tile* TunnelAlgorithm::GenerateLevel() {
 		int x = rng->getInt(0, mWidth - w - 1);
 		int y = rng->getInt(0, mHeight - h - 1);
 
-		Rect* nRoom = new Rect(x,y,w, h);
+		Rect nRoom = Rect(x,y,w, h);
 
 		bool rGFail = false;
 
 		for (int j = 0; j < numRooms; ++j){
-			if (nRoom->intersects(rooms[j])) {
+			if (nRoom.intersects(rooms[j])) {
 				rGFail = true;
 				break;
 			}
@@ -46,12 +46,12 @@ Tile* TunnelAlgorithm::GenerateLevel() {
 		 
 		if (!rGFail) {
 			createRoom(nRoom);
-			int newX = nRoom->centerX;
-			int newY = nRoom->centerY;
+			int newX = nRoom.centerX;
+			int newY = nRoom.centerY;
 
 			if (numRooms != 0) {
-				int prevX = rooms[numRooms - 1]->centerX;
-				int prevY = rooms[numRooms - 1]->centerY;
+				int prevX = rooms[numRooms - 1].centerX;
+				int prevY = rooms[numRooms - 1].centerY;
 
 				if (rng->getInt(0, 1) == 1) {
 					createHTunnel(prevX, newX, prevY);
@@ -70,11 +70,11 @@ Tile* TunnelAlgorithm::GenerateLevel() {
 	return mtiles;
 }
 
-void TunnelAlgorithm::createRoom(Rect* room){
-	int x = room->x1 +1 ;
-	int y = room->y1 +1;
-	for (x; x < room->x2; ++x) {
-		for (y; y < room->y2; ++y) {
+void TunnelAlgorithm::createRoom(Rect& room){
+	int x = room.x1 +1 ;
+	int y = room.y1 +1;
+	for (x; x < room.x2; ++x) {
+		for (y; y < room.y2; ++y) {
 			mtiles[x + (y*mWidth)].canWalk = true;
 		}
 	}
